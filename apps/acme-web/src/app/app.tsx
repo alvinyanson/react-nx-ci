e1c06cb0f084b5b93884b15312194b1514dc86b1
alvinyanson <a.yanson@arcanys.com>
2025-05-05 21:02:13 +0800

    initial

---
+++ apps/acme-web/src/app/app.tsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
+import { Products } from '@react-nx-cicd/products';
+import { Orders } from '@react-nx-cicd/orders';
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<Products />}></Route>
+      <Route path="/orders" element={<Orders />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
