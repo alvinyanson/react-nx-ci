ef780571976a4ea07ead928c1263eb7acb9f52ff
alvinyanson <a.yanson@arcanys.com>
2025-05-05 21:05:47 +0800

    modified product lib

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -3,7 +3,7 @@ import styles from './products.module.scss';
 export function Products() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Products!</h1>
+      <h2>Welcome to Products!</h2>
     </div>
   );
 }
